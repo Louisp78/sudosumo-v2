8f3274db3eee00e42f204bce5c7c3e7db37cc3fd
Louis Place <louis.place@epita.fr>
2024-09-18 14:15:39 +0200

    chore(auth): redirection missing

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation"
 import { getUser } from "../service/user"
 import NoSpoonLeftPage from "./NoSpoonLeftPage"
 import PuzzlePage from "./PuzzlePage"
@@ -5,6 +6,8 @@ import PuzzlePage from "./PuzzlePage"
 export default async function HomePage() {
 
     const user = await getUser()
+    if (user == null)
+        redirect('/login')
     if (user?.lifes)
         return <PuzzlePage />
     else
