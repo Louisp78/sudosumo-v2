56f7be23b9fc6b488ac71ec4ef8078470d4ee5de
Louis Place <louis.place@epita.fr>
2024-09-18 15:12:17 +0200

    fix(auth): try with sign in btn follow docs

--- src/app/components/GoogleSignInBtn.tsx
+++ src/app/components/GoogleSignInBtn.tsx
@@ -1,9 +1,14 @@
-import { signInAction } from "../service/actions"
 import { FaGoogle } from "react-icons/fa"
+import { signIn } from "../auth"
 
 export default function GoogleSignInBtn() {
   return (
-    <form action={signInAction}>
+    <form action={
+      async () => {
+        "use server"
+        await signIn("google")
+      }
+    }>
       <button type="submit" className="border px-3 py-2 rounded-sm font-medium flex gap-3 items-center">
         <FaGoogle />
         Signin with Google
