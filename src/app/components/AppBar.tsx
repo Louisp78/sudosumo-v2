fdd745d65b70ebd23634773d7d662beacea7b129
Louis Place <louis.place@epita.fr>
2024-09-19 11:19:22 +0200

    fix(appbar): ux ui

--- src/app/components/AppBar.tsx
+++ src/app/components/AppBar.tsx
@@ -6,32 +6,48 @@ import { Button } from "./ui/button"
 import UserAvatar from "./UserAvatar"
 import { usePathname } from "next/navigation"
 import { GiSpoon } from "react-icons/gi"
+import { Tooltip, TooltipProvider } from "@/components/ui/tooltip"
+import { TooltipContent, TooltipTrigger } from "@radix-ui/react-tooltip"
 
 export default function AppBar(props: { avatarUrl?: string | null, lifes: number }) {
     const pathname = usePathname()
     return (
-        <header className="bg-white bg-opacity-90 backdrop-blur-sm shadow-md py-4 px-6">
-            <div className="max-w-7xl mx-auto flex justify-between items-center">
-                <h1 className="text-2xl font-bold text-orange-700 font-japanese flex items-center">
-                    <SoupIcon className="mr-2 text-yellow-500" />
-                    SudoSumo
-                </h1>
-                <nav className="flex items-center space-x-4">
-                    {pathname === "/profile" &&
-                        <Link href={"/"}>
-                            <Button variant="ghost" className="bg-opacity-80 text-orange-700 flex gap-3">
-                                <ArrowLeftIcon />
-                                Go back to puzzle
-                            </Button>
-                        </Link>
-                    }
-                    <div className="flex items-center space-x-2">
-                        <GiSpoon className="text-red-500" size={23} />
-                        <span className='text-xl font-semibold text-red-500'>{props.lifes}</span>
-                    </div>
-                    <Link href={"/profile"}>
-                        <UserAvatar size={50} className="w-10 h-10" avatarUrl={props.avatarUrl} />
+        <header className="bg-white bg-opacity-90 backdrop-blur-sm shadow-md py-2 px-6">
+            <div className="flex justify-evenly items-center">
+                {pathname !== "/profile" && <Link href={"/"}>
+                    <h1 className="text-2xl font-bold text-orange-700 font-japanese flex items-center">
+                        <SoupIcon className="mr-2 text-yellow-500" />
+                        SudoSumo
+                    </h1>
+                </Link>
+                }
+                {pathname === "/profile" &&
+                    <Link href={"/"}>
+                        <Button variant="ghost" className="bg-opacity-80 text-orange-700 flex gap-3">
+                            <ArrowLeftIcon />
+                            Go back to puzzle
+                        </Button>
                     </Link>
+                }
+                <nav className="flex gap-3 md:gap-7">
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <button className="flex items-center space-x-2 px-2 md:hover:bg-red-500 text-red-500 md:hover:text-white rounded-sm">
+                                    <GiSpoon className="" size={25} />
+                                    <span className='text-xl font-semibold'>{props.lifes}</span>
+                                </button>
+                            </TooltipTrigger>
+                            <TooltipContent className="hidden md:block bg-white rounded-md p-2">
+                                <p>{"Every day, your lives are reset to 5"}</p>
+                                <p>Use them wisely to solve puzzles!</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                    <Link href={"/profile"}>
+                        <UserAvatar size={50} className="w-12 h-12" avatarUrl={props.avatarUrl} />
+                    </Link>
+
                 </nav>
             </div>
         </header>
