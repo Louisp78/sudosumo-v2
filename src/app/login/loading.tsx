de49277429b5d7f457121946f3db13081f5b0696
Louis Place <louis.place@epita.fr>
2024-09-20 12:24:50 +0200

    feat(login): update temp loading

--- src/app/login/loading.tsx
+++ src/app/login/loading.tsx
@@ -1,6 +1,8 @@
 //TODO: add loader
 export default function LoadingPage() {
     return (
-        <p>Loading ...</p>
+        <main className="flex flex-grow w-full justify-center items-center">
+            <p className="text-white text-2xl">Loading ...</p>
+        </main>
     )
 }
